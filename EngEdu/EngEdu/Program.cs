2cd5821b64a3aa0aaa4bd79cff69c8cde1a74797
win <win@DESKTOP-MKV5LJJ>
2017-08-31 13:48:01 +0700

    Edit year 2560

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -10,9 +10,9 @@ namespace EngEdu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Year 225x semester 1,I am an Enged Freshman");
-            Console.WriteLine("Year 225x semester 2,I learn to program in C sharp");
-            Console.WriteLine("Year 225x semester 1,I start my programmer life");
+            Console.WriteLine("Year 2560 semester 1,I am an Enged Freshman");
+            Console.WriteLine("Year 2560 semester 2,I learn to program in C sharp");
+            Console.WriteLine("Year 2560 semester 1,I start my programmer life");
 
         }
     }
